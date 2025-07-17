df8b6912aa29c7c32174ad0c0970d7afe7111023
Muna Faith <munafaith294@gmail.com>
2025-07-17 13:45:59 +0300

    Fix input prompt for weather_advice.py

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,4 +1,4 @@
-current_weather=str(input("What's the weather like today? (sunny/rainy/cold)" )).lower()
+current_weather=str(input("What's the weather like today? (sunny/rainy/cold):" )).lower()
 
 if current_weather=="sunny":
     print("Wear a t-shirt and sunglasses.")
